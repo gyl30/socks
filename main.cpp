74041fa664d318952bc79df58628c0ba6efbc2e2
gyl30 <im.ggyyll@gmail.com>
2026-01-17 20:11:45 +0800

    feat(cli): 支持生成 X25519 密钥对

--- main.cpp
+++ main.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <string>
 #include <thread>
-#include <boost/asio.hpp>
+#include <vector>
+#include <asio.hpp>
 #include "log.h"
 #include "local_client.h"
 #include "remote_server.h"
 #include "context_pool.h"
+#include "crypto_util.h"
 
 static void print_usage(const char* prog)
 {
     std::cout << "usage\n";
     std::cout << "  run as local client " << prog << " -c <remote_host> <remote_port> <local_port> <auth_key_hex> <sni>\n";
     std::cout << "  run as remote server " << prog << " -s <bind_port> <fallback_host> <fallback_port> <auth_key_hex>\n";
+    std::cout << "  generate key pair    " << prog << " -g\n";
 }
 
 int main(int argc, char** argv)
 {
     const std::string app_name(argv[0]);
-    init_log(app_name + ".log");
 
     if (argc < 2)
     {
@@ -26,6 +28,29 @@ int main(int argc, char** argv)
     }
 
     const std::string mode = argv[1];
+
+    if (mode == "-g")
+    {
+        uint8_t pub[32];
+        uint8_t priv[32];
+        reality::crypto_util::generate_x25519_keypair(pub, priv);
+        const std::vector<uint8_t> vec_priv(priv, priv + 32);
+        const std::vector<uint8_t> vec_pub(pub, pub + 32);
+
+        std::cout << "Generated X25519 Keypair (Hex):" << std::endl;
+        std::cout << "----------------------------------------------------------------" << std::endl;
+        std::cout << "Private Key: " << reality::crypto_util::bytes_to_hex(vec_priv) << std::endl;
+        std::cout << "Public Key:  " << reality::crypto_util::bytes_to_hex(vec_pub) << std::endl;
+        std::cout << "----------------------------------------------------------------" << std::endl;
+        std::cout << "Usage:" << std::endl;
+        std::cout << "  Server: Use 'Private Key' for authentication." << std::endl;
+        std::cout << "  Client: Use 'Public Key' to connect." << std::endl;
+
+        return 0;
+    }
+
+    init_log(app_name + ".log");
+
     const auto threads_count = std::thread::hardware_concurrency();
     std::error_code ec;
     io_context_pool pool(threads_count > 0 ? threads_count : 4, ec);
@@ -87,8 +112,6 @@ int main(int argc, char** argv)
         {
             if (!error)
             {
-                LOG_INFO("received signal {} stopping...", signal_number);
-
                 if (client)
                 {
                     client->stop();
