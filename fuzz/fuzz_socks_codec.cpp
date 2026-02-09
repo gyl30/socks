2de69a72f7d3ea4fc52c2dd769866c667b4ef0ad
gyl30 <im.ggyyll@gmail.com>
2026-02-09 12:01:22 +0800

    chore(fuzz): 添加 fuzz 相关测试与忽略规则

--- fuzz/fuzz_socks_codec.cpp
+++ fuzz/fuzz_socks_codec.cpp
@@ -2,14 +2,15 @@
 #include <cstdint>
 #include <stddef.h>
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
-{
 #include "protocol.h"
 
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
     socks_udp_header header;
     if (socks_codec::decode_udp_header(data, size, header))
     {
-        socks_codec::encode_udp_header(header);
+        auto bytes = socks_codec::encode_udp_header(header);
+        (void)bytes;
     }
     return 0;
 }
