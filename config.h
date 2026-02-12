0b5262ccc0a9102ffe71a5825da31392fd4a88cf
gyl30 <im.ggyyll@gmail.com>
2026-02-12 21:16:27 +0800

    feat(config): 支持重放缓存容量配置并补充解析测试

--- config.h
+++ config.h
@@ -107,6 +107,7 @@ struct config
         std::string dest;
         std::string type = "tcp";
         bool strict_cert_verify = true;
+        std::uint32_t replay_cache_max_entries = 100000;
         std::string private_key = "b0c338c6353fab820a0e5d16b6fcf41ee4166940795f89d0cde8902675ce9456";
         std::string public_key = "8d4e6ddf1479f2305b6645f045e02f9f5e400005884a8f1663ee9c51915bcc6d";
         std::string short_id;
