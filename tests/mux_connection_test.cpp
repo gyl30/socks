4ff8b5c7175d1a75ac043ba3ce8fc5cf05d3907f
gyl30 <im.ggyyll@gmail.com>
2026-02-15 20:14:01 +0800

    test(stop): 补充mux connection stop队列阻塞回归

--- tests/mux_connection_test.cpp
+++ tests/mux_connection_test.cpp
@@ -515,6 +515,64 @@ TEST_F(mux_connection_integration_test, StopRunsWhenIoContextNotRunning)
     EXPECT_EQ(conn->connection_state_.load(std::memory_order_acquire), mux_connection_state::kClosed);
 }
 
+TEST_F(mux_connection_integration_test, StopRunsWhenIoQueueBlocked)
+{
+    auto conn = std::make_shared<mux_connection>(
+        asio::ip::tcp::socket(io_ctx()), io_ctx(), reality_engine{{}, {}, {}, {}, EVP_aes_128_gcm()}, true, 18);
+    conn->started_.store(true, std::memory_order_release);
+    conn->connection_state_.store(mux_connection_state::kConnected, std::memory_order_release);
+
+    auto stream = std::make_shared<simple_mock_stream>();
+    conn->streams_[303] = stream;
+
+    std::atomic<bool> blocker_started{false};
+    std::atomic<bool> release_blocker{false};
+    asio::post(
+        io_ctx(),
+        [&blocker_started, &release_blocker]()
+        {
+            blocker_started.store(true, std::memory_order_release);
+            while (!release_blocker.load(std::memory_order_acquire))
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+        });
+
+    std::thread io_thread([this]() { io_ctx().run(); });
+    bool started = false;
+    for (int i = 0; i < 100; ++i)
+    {
+        if (blocker_started.load(std::memory_order_acquire))
+        {
+            started = true;
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+    if (!started)
+    {
+        release_blocker.store(true, std::memory_order_release);
+        io_ctx().stop();
+        if (io_thread.joinable())
+        {
+            io_thread.join();
+        }
+        FAIL();
+    }
+
+    conn->stop();
+    EXPECT_EQ(conn->connection_state_.load(std::memory_order_acquire), mux_connection_state::kClosed);
+    EXPECT_TRUE(conn->streams_.find(303) == conn->streams_.end());
+    EXPECT_TRUE(stream->reset());
+
+    release_blocker.store(true, std::memory_order_release);
+    io_ctx().stop();
+    if (io_thread.joinable())
+    {
+        io_thread.join();
+    }
+}
+
 TEST_F(mux_connection_integration_test, RemoveStreamRunsWhenIoContextNotRunning)
 {
     config::limits_t limits_cfg;
