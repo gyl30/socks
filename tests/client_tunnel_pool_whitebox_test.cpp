9181ea3feaab083265b864574ae846b1668ed2a7
gyl30 <im.ggyyll@gmail.com>
2026-02-15 20:38:45 +0800

    test(stop): 补充client tunnel pool stop已停止态清理回归

--- tests/client_tunnel_pool_whitebox_test.cpp
+++ tests/client_tunnel_pool_whitebox_test.cpp
@@ -381,6 +381,34 @@ TEST(ClientTunnelPoolWhiteboxTest, StopClosesPendingSocketWhenIoContextNotRunnin
     EXPECT_EQ(tunnel_pool->pending_sockets_[0], nullptr);
 }
 
+TEST(ClientTunnelPoolWhiteboxTest, StopClosesPendingSocketWhenIoContextStopped)
+{
+    std::error_code ec;
+    mux::io_context_pool pool(1);
+    ASSERT_FALSE(ec);
+
+    auto cfg = make_base_cfg();
+    cfg.reality.public_key = generate_public_key_hex();
+    auto tunnel_pool = std::make_shared<mux::client_tunnel_pool>(pool, cfg, 0);
+
+    asio::io_context tunnel_io_context;
+    auto pending_socket = std::make_shared<asio::ip::tcp::socket>(tunnel_io_context);
+    pending_socket->open(asio::ip::tcp::v4(), ec);
+    ASSERT_FALSE(ec);
+    ASSERT_TRUE(pending_socket->is_open());
+
+    tunnel_pool->pending_sockets_.resize(1);
+    tunnel_pool->pending_sockets_[0] = pending_socket;
+    tunnel_pool->tunnel_io_contexts_.resize(1, &tunnel_io_context);
+
+    tunnel_io_context.stop();
+    tunnel_pool->stop();
+
+    EXPECT_TRUE(tunnel_pool->stop_.load(std::memory_order_acquire));
+    EXPECT_FALSE(pending_socket->is_open());
+    EXPECT_EQ(tunnel_pool->pending_sockets_[0], nullptr);
+}
+
 TEST(ClientTunnelPoolWhiteboxTest, StopClosesPendingSocketWhenIoQueueBlocked)
 {
     std::error_code ec;
