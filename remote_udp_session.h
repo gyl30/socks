7060fc9b7fb9f622bc8bdbed0e58e7999f49a7cb
gyl30 <im.ggyyll@gmail.com>
2026-02-25 15:03:28 +0800

    fix(remote_udp_session): 双栈初始化失败时回退ipv4并补充回归测试

--- remote_udp_session.h
+++ remote_udp_session.h
@@ -71,6 +71,7 @@ class remote_udp_session : public mux_stream_interface, public std::enable_share
     boost::asio::ip::udp::socket udp_socket_;
     boost::asio::ip::udp::resolver udp_resolver_;
     std::weak_ptr<mux_connection> connection_;
+    bool udp_socket_use_v6_ = true;
     std::atomic<bool> terminated_{false};
     std::atomic<bool> cleaned_up_{false};
     std::atomic<std::uint64_t> last_read_time_ms_{0};
