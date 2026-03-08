c52097b8e968627d6150d33e6a3c01b21ba22285
gyl30 <im.ggyyll@gmail.com>
2026-03-08 10:11:20 +0800

    fix(socks5): 绑定 udp associate 到控制连接来源

--- udp_socks_session.h
+++ udp_socks_session.h
@@ -101,6 +101,8 @@ class udp_socks_session : public std::enable_shared_from_this<udp_socks_session>
     bool proxy_stream_started_ = false;
     bool has_client_addr_ = false;
     boost::asio::ip::udp::endpoint client_addr_;
+    bool has_control_peer_addr_ = false;
+    boost::asio::ip::address control_peer_addr_;
     bool has_request_client_addr_ = false;
     boost::asio::ip::address request_client_addr_;
     bool has_request_client_port_ = false;
