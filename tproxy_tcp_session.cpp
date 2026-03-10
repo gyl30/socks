df134468b9d833be91dea5a23e94e41460613ac0
gyl30 <im.ggyyll@gmail.com>
2026-03-10 09:28:45 +0800

    fix(mux): 放宽代理 connect ack 等待

--- tproxy_tcp_session.cpp
+++ tproxy_tcp_session.cpp
@@ -231,7 +231,7 @@ boost::asio::awaitable<std::pair<route_type, std::shared_ptr<upstream>>> tproxy_
             LOG_CTX_WARN(ctx_, "{} tunnel pool unavailable for proxy route", log_event::kRoute);
             co_return std::make_pair(route_type::kBlock, std::shared_ptr<upstream>(nullptr));
         }
-        const std::shared_ptr<upstream> backend = std::make_shared<proxy_upstream>(tunnel_pool_, io_context_, ctx_);
+        const std::shared_ptr<upstream> backend = std::make_shared<proxy_upstream>(tunnel_pool_, io_context_, ctx_, cfg_);
         co_return std::make_pair(route, backend);
     }
     co_return std::make_pair(route_type::kBlock, std::shared_ptr<upstream>(nullptr));
